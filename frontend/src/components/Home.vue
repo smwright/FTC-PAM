636f1c75504900d647fc270556979bdeb76902bd
Rafael <rafael.grote@gmx.net>
2018-05-22 21:55:28 +0200

    frontend adjustments for local development

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,28 +1,28 @@
 <template>
   <div class="home">
     <h1>
-    Welcome to Air Combat Group
+    Welcome to Air Combat Group awesome
     </h1>
     <p>
-    Air Combat Group consists of both Allied and Axis squadrons under a single operational 
-    umbrella flying with full realism settings. We are an English and German speaking 
-    group located in Europe and the Americas and meet on our own Teamspeak 3 server. 
-    Our pilots are interested in the history of World War II air combat on all fronts, 
-    seeking immersive air combat in the theatres of the time through historical 
-    campaign and other online activities. By supporting both sides of the conflict 
-    we can create parity in theatre and achieve common ground across the group, 
-    ultimately we seek to mirror the numbers flying on each side and support increasingly 
+    Air Combat Group consists of both Allied and Axis squadrons under a single operational
+    umbrella flying with full realism settings. We are an English and German speaking
+    group located in Europe and the Americas and meet on our own Teamspeak 3 server.
+    Our pilots are interested in the history of World War II air combat on all fronts,
+    seeking immersive air combat in the theatres of the time through historical
+    campaign and other online activities. By supporting both sides of the conflict
+    we can create parity in theatre and achieve common ground across the group,
+    ultimately we seek to mirror the numbers flying on each side and support increasingly
     large campaigns.
     </p>
     <p>
     Our objective is to provide an unpressurised, non-competitive
-    and friendly environment in which to enjoy combat operations, whichever side 
+    and friendly environment in which to enjoy combat operations, whichever side
     a pilot prefers, and all standards are welcome regardless of experience. The
     squadrons within the group are historically based and the missions we fly are
     selected for their historical accuracy whether on our public server or in campaign.
     Group and squadron objectives are to re-enact the history ahead of competition,
     to attempt to be a part of being there, to learn about some of the experiences
-    that were faced by the young pilots who fought for their countries in World 
+    that were faced by the young pilots who fought for their countries in World
     War II and to honour them by understanding more about what they faced.
     </p>
     <p>
