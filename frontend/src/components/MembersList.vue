4f061df524a08dee8712a245917044ff38ea7844
Rafael <rafael.grote@gmx.net>
2018-06-02 00:09:45 +0200

    adding mockup data load for memberlist

--- frontend/src/components/MembersList.vue
+++ frontend/src/components/MembersList.vue
@@ -1,52 +1,65 @@
 <template>
-  <div class="all-members">
+  <div class="MembersList">
     <h1>Members</h1>
     <p>These pages list all registered ACG members with their corresponding names, callsigns, joining- and eventually discharge dates, their status and their current squadron. Click on any member to access a detailed profile.</p>
     <table>
       <tr>
-        <th>Rank</th>
+        <th>ID</th>
+        <th>Name</th>
         <th>Callsign</th>
-        <th>Joining Date</th>
-        <th>Last status change</th>
-        <th>Current Status</th>
-        <th>Current Squadron/Staffel</th>
+        <th>Admin</th>
+        <th>Map Viewer</th>
+        <th>Mission Builder</th>
       </tr>
       <tr class="link" v-for="member in members" v-on:click="routeToMember(member.member_id)">
-        <td>{{member.rank}}</td>
+        <td>{{member.id}}</td>
+        <td>{{member.username}}</td>
         <td>{{member.callsign}}</td>
-        <td>{{member.joining_date}}</td>
-        <td>{{member.last_status_change}}</td>
-        <td>{{member.current_status}}</td>
-        <td>{{member.current_squadron}}</td>
+        <td>{{member.admin}}</td>
+        <td>{{member.mapViewer}}</td>
+        <td>{{member.missionBuilder}}</td>
       </tr>
     </table>
   </div>
 </template>
 
 <script>
-import Member from './../resource/member'
+import Mockcom from './../resource/index'
 
 export default {
-  name: 'AllMembers',
-  created: function () {
-    this.getMembers()
+  name: 'MembersList',
+  mounted () {
+    Mockcom.getByName('members')
+      .then(response => {
+       return this.addCampaigns(response);
+      })
+      .then(response => {
+        this.members = response;
+      })
+      .catch(function (error) {
+        console.log(error.message);
+      })
+
   },
   data () {
     return {
-      members: []
+      members: null
     }
   },
   methods: {
-    getMembers: function () {
-      Member.getAll().then((data) => {
-        this.members = data;
-      }, (err) => {
-        console.log(err)
-      })
-    },
-    // routeToMember: function (memberID) {
-    //   this.$router.push({name: 'Member', params: {member_id: memberID}})
-    // }
+    addCampaigns: async function (members) {
+
+      for(var i = 0; i < members.length; i++) {
+        var m_id = members[i].id % 3 + 1;
+        var campaign_call = Mockcom.getByName('members_c_info_' + m_id);
+        try {
+          members[i].campaigns = await campaign_call;
+        } catch (err) {
+          console.log(err.message)
+        }
+      }
+      return members;
+    }
   }
 }
 </script>
