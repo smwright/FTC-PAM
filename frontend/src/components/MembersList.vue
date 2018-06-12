2857f58548177dc750b2244b4e02b1936f5ca138
Rafael <rafael.grote@gmx.net>
2018-06-12 22:28:39 +0200

    cont. work on members-list. Including JSON template and backend data demand

--- frontend/src/components/MembersList.vue
+++ frontend/src/components/MembersList.vue
@@ -2,24 +2,25 @@
   <div class="MembersList">
     <h1>Members</h1>
     <p>These pages list all registered ACG members with their corresponding names, callsigns, joining- and eventually discharge dates, their status and their current squadron. Click on any member to access a detailed profile.</p>
-    <table>
-      <tr>
-        <th>ID</th>
-        <th>Name</th>
-        <th>Callsign</th>
-        <th>Admin</th>
-        <th>Map Viewer</th>
-        <th>Mission Builder</th>
-      </tr>
-      <tr class="link" v-for="member in members" v-on:click="routeToMember(member.member_id)">
-        <td>{{member.id}}</td>
-        <td>{{member.username}}</td>
-        <td>{{member.callsign}}</td>
-        <td>{{member.admin}}</td>
-        <td>{{member.mapViewer}}</td>
-        <td>{{member.missionBuilder}}</td>
-      </tr>
-    </table>
+    <div class="link" v-for="(member, id) in members" v-on:click="routeToMember(member.id)">
+      <div class="callsign">
+        {{ member.callsign}}
+      </div>
+      <div class="two-line-link-button">
+        <div>
+          <span>Joining date: {{ member.jdate }}</span>
+          <span>Current status: {{ currentStatus(member.cstatus) }}</span>
+        </div>
+        <div>
+          <span>Length of service: {{ member.length_of_service}}</span>
+          <span>Campaigns: {{ member.campaigns }}</span>
+          <span>Sorties: {{ member.sorties }}</span>
+        </div>
+      </div>
+      <div class="div-rank-value">
+        {{ member.rank_value }}
+      </div>
+    </div>
   </div>
 </template>
 
@@ -30,11 +31,12 @@ export default {
   name: 'MembersList',
   mounted () {
     Mockcom.getByName('members')
-      .then(response => {
-       return this.addCampaigns(response);
-      })
+      // .then(response => {
+      //  return this.addCampaigns(response);
+      // })
       .then(response => {
         this.members = response;
+        console.log(JSON.stringify(this.members))
       })
       .catch(function (error) {
         console.log(error.message);
@@ -47,18 +49,32 @@ export default {
     }
   },
   methods: {
-    addCampaigns: async function (members) {
-
-      for(var i = 0; i < members.length; i++) {
-        var m_id = members[i].id % 3 + 1;
-        var campaign_call = Mockcom.getByName('members_c_info_' + m_id);
-        try {
-          members[i].campaigns = await campaign_call;
-        } catch (err) {
-          console.log(err.message)
-        }
+    // addCampaigns: async function (members) {
+    //
+    //   for(var i = 0; i < members.length; i++) {
+    //     var m_id = members[i].id % 3 + 1;
+    //     var campaign_call = Mockcom.getByName('members_c_info_' + m_id);
+    //     try {
+    //       members[i].campaigns = await campaign_call;
+    //     } catch (err) {
+    //       console.log(err.message)
+    //     }
+    //   }
+    //   return members;
+    // }
+    currentStatus: function(cstatus) {
+      switch (cstatus) {
+        case 0:
+          return "Active";
+        case 1:
+          return "Dismissed";
+        case 2:
+          return "Relieved from duty";
+        case 3:
+          return "Passed away";
+        default:
+          return cstatus;
       }
-      return members;
     }
   }
 }
@@ -66,4 +82,25 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .div-rank-value {
+    display: inline-block;
+    width: calc(10% - 4px); /* Whished widht in % minus 2x border size */
+    text-align: center;
+    vertical-align: center;
+  }
+
+  .two-line-link-button {
+    width: calc(90% - 4px); /* Whished widht in % minus 2x border size */
+    display: inline-block;
+  }
+
+  .two-line-link-button span {
+    margin: 10px;
+    width: auto;
+  }
+
+  .callsign{
+    font-weight: bold;
+    font-size: 1.2em;
+  }
 </style>
