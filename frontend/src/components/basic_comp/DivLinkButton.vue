8106e699365599194e45c6b4ff661588112a154c
Rafael <rafael.grote@gmx.net>
2019-02-26 23:55:14 +0100

    Adding basic helper components (DivLinkButton, HideableDiv)

---
+++ frontend/src/components/basic_comp/DivLinkButton.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="div-button" v-on:click="routeTo()">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DivLinkButton",
+  props: {
+    routeName: {
+      type: String,
+      default: "/"
+    },
+    routeParams: {
+      type: Object,
+      default: null
+    }
+  },
+  methods: {
+    routeTo: function () {
+      this.$router.push({name: this.routeName, params: this.routeParams});
+      //DEBUG
+      console.log(this.$parent.$options.name+": Pushing to -> {name: "+this.routeName+", params: "+JSON.stringify(this.routeParams)+"}");
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.div-button{
+  margin: 10px 2px 10px 2px;
+  cursor: pointer;
+}
+
+.div-button:hover {
+  background: gray;
+}
+
+</style>
