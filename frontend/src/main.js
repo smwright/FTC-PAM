e9a2ce66ddc13650b916026f66fc1956319e2199
Rafael <rafael.grote@gmx.net>
2019-02-27 00:08:10 +0100

    Changes to main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,8 +4,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
+import session from 'vue-session'
+import auth from './resource/auth'
+import dbCon from './resource/dbConnector'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(session, {persist: true});
+Vue.mixin(auth);
+Vue.mixin(dbCon);
 
 /* eslint-disable no-new */
 var vm = new Vue({
