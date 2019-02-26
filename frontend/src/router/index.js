028550d51fd60685fe90e0e95d1531bcfdecbf52
Rafael <rafael.grote@gmx.net>
2019-02-27 00:07:01 +0100

    Adding new routes to router/index

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,19 +1,25 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Frontpage from '@/components/Frontpage'
-
 
 //About aCG
 import AboutUs from '@/components/AboutACGAboutUs'
 import AboutACGSideNav from '@/components/AboutACGSideNav'
 import Ranks from '@/components/AboutACGRanks'
 
-import CampaignList from '@/components/CampaignList'
-import CampaignInfoMain from '@/components/CampaignInfoMain'
-import CampaignInfoMissions from '@/components/CampaignInfoMissions'
-import CampaignInfoUnits from '@/components/CampaignInfoUnits'
-import CampaignInfoSideNav from '@/components/CampaignInfoSideNav'
+
+//Campaign
+import CampaignList from '../components/campaign/CampaignList'
+import CampaignInfoMain from '../components/campaign/CampaignInfoMain'
+import CampaignInfoMission from '../components/campaign/Mission'
+import CampaignInfoMissionLobby from '../components/campaign/MissionLobby'
+import CampaignInfoMissionBriefing from '../components/campaign/Briefing'
+import CampaignInfoUnits from '../components/campaign/Unit'
+import CampaignInfoSideNav from '../components/campaign/SideNav'
+import CampaignInfoMissionUnitsSideNav from '../components/campaign/MissionUnitSideNav'
+import Report from '../components/campaign/Report'
+
 import CampaignEditor from '@/components/CampaignEditor'
+
 
 //Flight school components
 import FlightSchoolSideNav from '@/components/FlightSchoolSideNav'
@@ -21,17 +27,8 @@ import FlightSchoolMain from '@/components/FlightSchoolMain'
 import FlightSchoolFirstLesson from '@/components/FlightSchoolFirstLesson'
 import FlightSchoolSecondLesson from '@/components/FlightSchoolSecondLesson'
 
-import Missions from '@/components/Missions'
-import Admin from '@/components/Admin'
-import SquadronCommand from '@/components/SquadronCommand'
-import CampaignStatistics from '@/components/CampaignStatistics'
 import Home from '@/components/Home'
 
-//Member components
-import MembersList from '@/components/MembersList'
-import Member from '@/components/Member'
-import CharactersList from '@/components/CharactersList'
-import Character from '@/components/Character'
 
 Vue.use(VueRouter)
 
@@ -77,20 +74,51 @@ export default new VueRouter({
       },
       children: [
         {
-          path: '/campaign-info/units/:campaign_id',
+          path: 'units',
           name: 'Units',
           components: {
             subcontent: CampaignInfoUnits
           }
         },
         {
-          path: '/campaign-info/missions/:campaign_id',
+          path: 'missions',
           name: 'Missions',
           components: {
-            subcontent: CampaignInfoMissions
-          }
-        }
-      ]
+            subcontent: CampaignInfoMission
+          },
+        },
+        {
+          path: 'missions/:mission_id',
+          name: 'MissionInfo',
+          components: {
+            subcontent: CampaignInfoMissionLobby,
+            sideview_units: CampaignInfoMissionUnitsSideNav
+          },
+          children: [
+            {
+              path: 'briefing/:briefing_faction',
+              name: 'Briefing',
+              components: {
+                mission_lobby_content: CampaignInfoMissionBriefing
+              }
+            },
+            {
+              path: 'synop/:depl_unit_id',
+              name: 'MissionSynop',
+              components: {
+                mission_lobby_content: CampaignInfoMissionBriefing
+              }
+            },
+            {
+              path: 'report/:report_id',
+              name: 'Report',
+              components: {
+                mission_lobby_content: Report
+              }
+            }
+          ]
+        },
+      ],
     },
     {
       path: '/campaign-editor',
@@ -123,58 +151,5 @@ export default new VueRouter({
         }
       ]
     },
-    {
-      path: '/members',
-      name: 'Members',
-      component: MembersList
-      // children: [
-      //   {
-      //     path: '',
-      //     name: 'Members',
-      //     redirect: 'members-list'
-      //   },
-      //   {
-      //     path: 'members-list',
-      //     name: 'MembersList',
-      //     component: MembersList
-      //   },
-      //   {
-      //     path: 'members-list/:squadron',
-      //     name: 'MembersListSqn',
-      //     component: MembersList
-      //   },
-      //   {
-      //     path: 'characters-list',
-      //     name: 'AllCharacters',
-      //     component: CharactersList
-      //   },
-      //   {
-      //     path: 'member/:member_id',
-      //     name: 'Member',
-      //     component: Member
-      //   },
-      //   {
-      //     path: 'character/:character_id',
-      //     name: 'Character',
-      //     component: Character
-      //   }
-      //
-      // ]
-    },
-    {
-      path: '/missions',
-      name: 'Missions',
-      component: Missions
-    },
-    {
-      path: '/admin',
-      name: 'Admin',
-      component: Admin
-    },
-    {
-      path: '/sqncmd',
-      name: 'SquadronCommand',
-      component: SquadronCommand
-    }
   ]
 })
