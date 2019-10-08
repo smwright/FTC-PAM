cf4358b4c0f9c0473bbf32aed4643e12909d003a
Stef <smwright@hotmail.gr>
2019-10-08 17:48:09 +0300

    Home Page Reformat

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,37 +1,24 @@
 <template>
-  <div clas="home">
-    <h1>
-    Welcome to Air Combat Group
-    </h1>
-    <p>
-    Air Combat Group consists of both Allied and Axis squadrons under a single operational
-    umbrella flying with full realism settings. We are an English and German speaking
-    group located in Europe and the Americas and meet on our own Teamspeak 3 server.
-    Our pilots are interested in the history of World War II air combat on all fronts,
-    seeking immersive air combat in the theatres of the time through historical
-    campaign and other online activities. By supporting both sides of the conflict
-    we can create parity in theatre and achieve common ground across the group,
-    ultimately we seek to mirror the numbers flying on each side and support increasingly
-    large campaigns.
-    </p>
-    <p>
-    Our objective is to provide an unpressurised, non-competitive
-    and friendly environment in which to enjoy combat operations, whichever side
-    a pilot prefers, and all standards are welcome regardless of experience. The
-    squadrons within the group are historically based and the missions we fly are
-    selected for their historical accuracy whether on our public server or in campaign.
-    Group and squadron objectives are to re-enact the history ahead of competition,
-    to attempt to be a part of being there, to learn about some of the experiences
-    that were faced by the young pilots who fought for their countries in World
-    War II and to honour them by understanding more about what they faced.
-    </p>
-    <p>
-    If this sounds of interest to you we are welcoming to new
-    pilots of any standard or experience, even if you've never flown before - all
-    you need is a PC with IL2 Cliffs of Dover (you need a decent PC) and a headset/microphone.
-    Just hop onto our TS3 comms at anytime, weekends are best, and join in the fun.
-    </p>
+  <div>
+  <div class="header">
+    <h1>Welcome to the Air Combat Group</h1>
   </div>
+    <div class="home">
+      <p>
+      The Air Combat Group consists of both Allied and Axis squadrons under a single operational umbrella flying with full realism settings. We are an English and German speaking group located in Europe, the Americas and Australia and meet on our own Teamspeak 3 and Discord servers. Our pilots are interested in the history of World War II air combat on all fronts, seeking immersive air combat in the theatres of the time through historical campaign and other online activities. By supporting both sides of the conflict we can create parity in theatre and achieve common ground across the group, ultimately we seek to mirror the numbers flying on each side and support increasingly large campaigns.
+    </p>
+    <p>
+      Our objective is to provide an unpressurised, non-competitive and friendly environment in which to enjoy combat operations, whichever side a pilot prefers, and all standards are welcome regardless of experience. The squadrons within the group are historically based and the missions we fly are selected for their historical accuracy whether on our public server or in campaign. Group and squadron objectives are to re-enact the history ahead of competition, to attempt to be a part of being there, to learn about some of the experiences that were faced by the young pilots who fought for their countries in World War II and to honour them by understanding more about what they faced.
+    </p>
+    <p>
+      If this sounds of interest to you we are welcoming to new pilots of any standard or experience, even if you've never flown before - all you need is a decent PC with IL-2 Sturmovik: Cliffs of Dover or IL-2 Sturmovik: Battle of Stalingrad (depending on the campaign being run) and a headset/microphone. Just hop onto our TS3 comms at anytime, weekends are best, and join in the fun.
+    </p>
+    <iframe id="ytplayer" type="text/html" width="930" height="518" src="https://www.youtube.com/embed/v6u4JxgE2S4?autoplay=1&origin=http://aircombatgroup.co.uk"
+            frameborder="0"></iframe>
+  </div>
+  </div>
+
+
 </template>
 
 <script>
@@ -75,9 +62,18 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
-div{
-  width: 100%;
+.home{
+  width: 60%;
+  text-align: justify;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.header{
+  width: 60%;
   text-align: center;
+  margin-left: auto;
+  margin-right: auto;
 }
 
 </style>
